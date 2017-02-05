0e0404421b675e013fe4cdb6350630a428941627
David Whitney <david_whitney@brown.edu>
2017-02-05 05:07:28 -0500

    before we fuck things up

---
+++ HackBrown/Assets/HAB/FollowFinger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowFinger : MonoBehaviour {
+
+	public GameObject toFollow;
+	public GameObject head;
+	public GameObject wine;
+	public float rotation; 
+	Color m;
+	Color w;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = toFollow.transform.position;
+		this.transform.rotation = toFollow.transform.rotation;
+		this.transform.Rotate (0, 0, rotation);
+		float dist = Vector3.Distance (this.transform.position, head.transform.position);
+		//Debug.Log (Vector3.Distance (this.transform.position, head.transform.position));
+		m = this.gameObject.GetComponent<Renderer> ().material.color;
+		m.a = Mathf.Lerp(0, 1f, (Mathf.InverseLerp(0.3f, 0.6f, dist)));
+		this.gameObject.GetComponent<Renderer> ().material.color = m;
+
+		w = wine.GetComponent<Renderer> ().material.color;
+		w.a = Mathf.Lerp(0, 1f, (Mathf.InverseLerp(0.3f, 0.6f, dist)));
+		wine.GetComponent<Renderer> ().material.color = w;
+	}
+}
--- HackBrown/Assets/HAB/Grab.cs
+++ HackBrown/Assets/HAB/Grab.cs
@@ -3,18 +3,47 @@ using System.Collections;
 
 public class Grab : MonoBehaviour {
 
+	public Mesh mesh;
 	// Use this for initialization
 	void Start () {
 	
 	}
 
 	void OnCollisionEnter(Collision collision) {
-		//Debug.Log (collision.gameObject.name);
-		if (collision.gameObject.name.Contains("palm") || collision.gameObject.name.Contains("bone3")) {
-			this.transform.parent = collision.gameObject.transform.parent.parent.FindChild ("palm");
-			this.transform.localPosition = new Vector3 (0f, -0.06f, 0f);
-			//this.transform.localEulerAngles = new Vector3 (90f, 90f, 0f);
-			Destroy (this.GetComponent<Rigidbody>());
+		Debug.Log (collision.gameObject.name);
+		if (collision.gameObject.name.Contains("bone")) {
+//			this.transform.parent = collision.gameObject.transform.parent.parent.FindChild ("palm");
+//			this.transform.localPosition = new Vector3 (0f, -0.06f, 0f);
+//			//this.transform.localEulerAngles = new Vector3 (90f, 90f, 0f);
+//			Destroy (this.GetComponent<Rigidbody>());
+			if (collision.gameObject.transform.parent.parent.name.Contains ("_L")) 
+			{
+				Transform hand = collision.gameObject.transform.parent.parent.parent.FindChild ("LoPoly_Rigged_Hand_Left").FindChild ("LoPoly_Hand_Mesh_Left");
+				hand.GetComponent<SkinnedMeshRenderer> ().enabled = false;
+				Transform tracked = hand.parent.parent.FindChild ("RigidRoundHand_L").FindChild ("index").FindChild ("bone3");
+				GameObject tracker = GameObject.Find ("LeftHandShotGlass");
+				tracker.transform.position = tracked.position;
+				tracker.transform.rotation = tracked.rotation;
+				tracker.GetComponent<MeshRenderer> ().enabled = true;
+				tracker.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = true;
+
+				//hand.GetComponent<SkinnedMeshRenderer> ().sharedMesh = mesh;
+				//hand.parent.localEulerAngles = new Vector3 (0, -90f, 0);
+			}
+			else if (collision.gameObject.transform.parent.parent.name.Contains ("_R")) 
+			{
+				Transform hand = collision.gameObject.transform.parent.parent.parent.FindChild ("LoPoly_Rigged_Hand_Right").FindChild ("LoPoly_Hand_Mesh_Right");
+				hand.GetComponent<SkinnedMeshRenderer> ().enabled = false;
+				Transform tracked = hand.parent.parent.FindChild ("RigidRoundHand_R").FindChild ("index").FindChild ("bone3");
+				GameObject tracker = GameObject.Find ("RightHandShotGlass");
+				tracker.transform.position = tracked.position;
+				tracker.transform.rotation = tracked.rotation;
+				tracker.GetComponent<MeshRenderer> ().enabled = true;
+				tracker.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = true;
+
+
+			}
+			Destroy (this.gameObject);
 		}
 
 	}

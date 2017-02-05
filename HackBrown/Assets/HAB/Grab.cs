bfce63983b65e3fc48dc2a590514a9f18551817d
David Whitney <david_whitney@brown.edu>
2017-02-05 02:45:22 -0500

    2:30 save game

---
+++ HackBrown/Assets/HAB/Grab.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grab : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		//Debug.Log (collision.gameObject.name);
+		if (collision.gameObject.name.Contains("palm") || collision.gameObject.name.Contains("bone3")) {
+			this.transform.parent = collision.gameObject.transform.parent.parent.FindChild ("palm");
+			this.transform.localPosition = new Vector3 (0f, -0.06f, 0f);
+			//this.transform.localEulerAngles = new Vector3 (90f, 90f, 0f);
+			Destroy (this.GetComponent<Rigidbody>());
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ HackBrown/Assets/HAB/TableGrab.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TableGrab : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		Debug.Log (collision.gameObject.name);
+		if (collision.gameObject.name.Contains("palm")) {
+			collision.transform.GetChild (0).parent = null;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
